90d6a9bdbd600e7c0b9247ac96e8e7e890bd7bd6
AzureDotnet <https://github.com/AzureDotnet>
2024-10-13 04:29:29 +0530

    first comminnnnt

--- obj/Debug/net8.0/helloworld.AssemblyInfo.cs
+++ obj/Debug/net8.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e14ccc7a01a2d85486c111a8c82a65ab11b656a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ba7ca575ff86916ecbca21847b73fbaef23f920")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
